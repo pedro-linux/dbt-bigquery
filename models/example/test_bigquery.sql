ebdc2f395fba52d9d34c2a29724df0e44b302cab
pedro-linux <badr.hilal@gmail.com>
2025-08-16 11:24:45 +0200

    Initial commit: dbt project setup

---
+++ models/example/test_bigquery.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='table') }}
+
+with customers as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from `dbt-tutorial`.jaffle_shop.customers
+
+)
+select * from customers
